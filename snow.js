bc619d3a4d936c2fd5c331766badd8456aef5c77
Inquieto <nicolanki036@gmail.com>
2026-05-05 03:52:12 -0300

    Update snow.js

--- snow.js
+++ snow.js
@@ -4,14 +4,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let mouse = {x:0, y:0};
+
+document.addEventListener("mousemove", e=>{
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
 let snow = [];
 
-for(let i=0;i<200;i++){
+for(let i=0;i<300;i++){
   snow.push({
     x:Math.random()*canvas.width,
     y:Math.random()*canvas.height,
-    r:Math.random()*2 + 1,
-    speed:Math.random()*1 + 0.5
+    r:Math.random()*3,
+    speed:Math.random()*1 + 0.5,
+    drift:Math.random()*1 - 0.5
   });
 }
 
@@ -26,6 +34,17 @@ function draw(){
     ctx.fill();
 
     f.y += f.speed;
+    f.x += f.drift;
+
+    // REAÇÃO AO MOUSE
+    let dx = f.x - mouse.x;
+    let dy = f.y - mouse.y;
+    let dist = Math.sqrt(dx*dx + dy*dy);
+
+    if(dist < 100){
+      f.x += dx / 10;
+      f.y += dy / 10;
+    }
 
     if(f.y > canvas.height){
       f.y = 0;
