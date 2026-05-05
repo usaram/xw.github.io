cc533d1e34ef94d4dd2e0536d467f2b286a56e0b
Inquieto <nicolanki036@gmail.com>
2026-05-05 04:09:56 -0300

    Refactor profile display and update logic

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const profilesDiv = document.getElementById("profiles");
-
-/* 🔥 EDITA AQUI */
 const profiles = [
   {
     id: "1466308940634652745",
@@ -8,46 +5,48 @@ const profiles = [
   }
 ];
 
-profiles.forEach(p => {
+const profilesDiv = document.getElementById("profiles");
+const discordBox = document.getElementById("discordBox");
 
-  const card = document.createElement("div");
-  card.className = "card";
+profiles.forEach(p=>{
 
-  card.innerHTML = `
+  const card=document.createElement("div");
+  card.className="card";
+
+  card.innerHTML=`
     <img class="avatar">
-    <div class="nick">${p.nick}</div>
+    <div>${p.nick}</div>
     <div class="username"></div>
     <div class="activity"></div>
   `;
 
   profilesDiv.appendChild(card);
 
-  const avatar = card.querySelector(".avatar");
-  const username = card.querySelector(".username");
-  const activity = card.querySelector(".activity");
+  const avatar=card.querySelector(".avatar");
+  const username=card.querySelector(".username");
+  const activity=card.querySelector(".activity");
 
   async function update(){
-    try{
-      const res = await fetch(`https://api.lanyard.rest/v1/users/${p.id}`);
-      const data = await res.json();
-      const user = data.data;
+    const res=await fetch(`https://api.lanyard.rest/v1/users/${p.id}`);
+    const data=await res.json();
+    const user=data.data;
 
-      username.innerText = user.discord_user.username;
+    username.innerText=user.discord_user.username;
 
-      avatar.src =
-        `https://cdn.discordapp.com/avatars/${p.id}/${user.discord_user.avatar}.png`;
+    avatar.src=`https://cdn.discordapp.com/avatars/${p.id}/${user.discord_user.avatar}.png`;
 
-      if(user.listening_to_spotify){
-        activity.innerText =
-          `${user.spotify.song} - ${user.spotify.artist}`;
-      } else {
-        activity.innerText =
-          user.activities[0]?.name || "Idle";
-      }
+    if(user.listening_to_spotify){
+      activity.innerText=`${user.spotify.song} - ${user.spotify.artist}`;
+    }else{
+      activity.innerText=user.activities[0]?.name || "Idle";
+    }
 
-    }catch{}
+    discordBox.innerHTML = `
+      ${user.discord_user.username}<br>
+      ${activity.innerText}
+    `;
   }
 
-  setInterval(update, 5000);
+  setInterval(update,5000);
   update();
 });
