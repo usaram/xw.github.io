641b37dd1f82815ff9f33bd163179d1b4b7bd074
Inquieto <nicolanki036@gmail.com>
2026-05-05 03:46:12 -0300

    Refactor Discord user update logic with profiles

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-const discordId = "1466308940634652745";
+const profilesDiv = document.getElementById("profiles");
 
-const activityEl = document.getElementById("activity");
-const discordBox = document.getElementById("discordInfo");
+/* 🔥 EDITA AQUI */
+const profiles = [
+  {
+    id: "SEU_ID_AQUI",
+    nick: "Seu Nick"
+  }
+];
 
-async function update(){
-  try{
-    const res = await fetch(`https://api.lanyard.rest/v1/users/${discordId}`);
-    const data = await res.json();
-    const user = data.data;
+profiles.forEach(p => {
 
-    if(user.listening_to_spotify){
-      activityEl.innerText =
-        `${user.spotify.song} - ${user.spotify.artist}`;
-    } else {
-      activityEl.innerText =
-        user.activities[0]?.name || "Idle";
-    }
+  const card = document.createElement("div");
+  card.className = "card";
 
-    discordBox.innerHTML = `
-      ${user.discord_user.username}<br>
-      ${activityEl.innerText}
-    `;
+  card.innerHTML = `
+    <img class="avatar">
+    <div class="nick">${p.nick}</div>
+    <div class="username"></div>
+    <div class="activity"></div>
+  `;
 
-  }catch{}
-}
+  profilesDiv.appendChild(card);
 
-setInterval(update,5000);
-update();
+  const avatar = card.querySelector(".avatar");
+  const username = card.querySelector(".username");
+  const activity = card.querySelector(".activity");
+
+  async function update(){
+    try{
+      const res = await fetch(`https://api.lanyard.rest/v1/users/${p.id}`);
+      const data = await res.json();
+      const user = data.data;
+
+      username.innerText = user.discord_user.username;
+
+      avatar.src =
+        `https://cdn.discordapp.com/avatars/${p.id}/${user.discord_user.avatar}.png`;
+
+      if(user.listening_to_spotify){
+        activity.innerText =
+          `${user.spotify.song} - ${user.spotify.artist}`;
+      } else {
+        activity.innerText =
+          user.activities[0]?.name || "Idle";
+      }
+
+    }catch{}
+  }
+
+  setInterval(update, 5000);
+  update();
+});
