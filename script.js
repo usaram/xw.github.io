52710494a2c092507edfb4dc3739ecd5e4b04f55
Inquieto <nicolanki036@gmail.com>
2026-05-14 08:06:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 // =========================
 
-window.addEventListener('mousemove', e => {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-let particles = [];
-
-for(let i = 0; i < 180; i++){
   particles.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     size: Math.random() * 2 + 0.5,
-    speedY: Math.random() * 0.7 + 0.2,
-    speedX: (Math.random() - 0.5) * 0.4
+    speedY: Math.random() * 0.5 + 0.15,
+    speedX: (Math.random() - 0.5) * 0.2
   });
 }
 
-function drawParticles(){
+function animateParticles(){
 
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
@@ -28,11 +20,11 @@ function drawParticles(){
     let dx = p.x - mouse.x;
     let dy = p.y - mouse.y;
 
-    let dist = Math.sqrt(dx * dx + dy * dy);
+    let dist = Math.sqrt(dx*dx + dy*dy);
 
     if(dist < 120){
-      p.x += dx * 0.015;
-      p.y += dy * 0.015;
+      p.x += dx * 0.01;
+      p.y += dy * 0.01;
     }
 
     p.y += p.speedY;
@@ -48,26 +40,29 @@ function drawParticles(){
     ctx.fill();
   });
 
-  requestAnimationFrame(drawParticles);
+  requestAnimationFrame(animateParticles);
 }
 
-drawParticles();
+animateParticles();
 
 // =========================
 // PERSONAGEM SEGUE MOUSE
 // =========================
 
-const character = document.getElementById('character');
+const anime = document.getElementById('anime');
 
 window.addEventListener('mousemove', e => {
 
-  const x = (window.innerWidth / 2 - e.clientX) / 50;
-  const y = (window.innerHeight / 2 - e.clientY) / 60;
+  const moveX = (window.innerWidth / 2 - e.clientX) / 60;
+  const moveY = (window.innerHeight / 2 - e.clientY) / 80;
 
-  character.style.transform = `translate(${x}px, ${y}px)`;
+  anime.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
+// =========================
 // RESPONSIVO
+// =========================
+
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
